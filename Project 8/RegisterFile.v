01f0e54e74f388a5167d0560ecb3d9cef80015db
Berkant-Simsek <126967154+Berkant-Simsek@users.noreply.github.com>
2023-03-20 16:23:37 +0300

    Create RegisterFile.v

---
+++ Project 8/RegisterFile.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+module RegisterFile(
+input[2:0] RS,
+input[2:0] RT,
+input[2:0] RD,
+input[15:0] WriteData,
+output[15:0] ReadRS,
+output[15:0] ReadRT,
+input RegWrite,
+input Clock);
+
+reg [15:0] Registers[7:0];
+
+always @(posedge Clock)
+begin  
+
+if(RegWrite)
+begin
+Registers[RD] <= WriteData;
+end
+
+end
+
+assign ReadRS = Registers[RS];
+assign ReadRT = Registers[RT];
+endmodule
